d73785d3dc98630e08a8d6d6e5f4695c9c9d5c4c
이현진 <qxzaxn123@gmail.com>
2023-05-19 14:18:14 +0900

    feat: 구분된 숫자가 6개가 아니면 예외처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,10 @@
 package lotto.model;
 
+import lotto.util.ExceptionMessage;
+
 import java.util.List;
+
+import static lotto.util.Constants.LOTTO_NUMBER_COUNT;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,12 +15,17 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        validateSize(numbers);
+        try{
+            validateSize(numbers);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private void validateSize(List<Integer> numbers){
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            if (numbers.size() != 6){
+                throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_SIZE.getMessage());
         }
     }
 }
--- src/main/java/lotto/util/ExceptionMessage.java
+++ src/main/java/lotto/util/ExceptionMessage.java
@@ -5,7 +5,9 @@ public enum ExceptionMessage {
 
     INVALID_MONEY_PRICE(String.format("%d원 단위로 구매 가능합니다.", LOTTO_PRICE)),
     INVALID_MONEY_NUMBER("자연수만 입력 가능합니다."),
-    DONT_EXIST_DELIMITER("구분자가 존재하지 않습니다.");
+    DONT_EXIST_DELIMITER("구분자가 존재하지 않습니다."),
+    INVALID_LOTTO_SIZE("6개의 숫자가 입력되지 않았습니다."),
+    DUPLICATE_LOTTO_NUMBER("중복된 숫자가 포함되어 있습니다.");
     private final String message;
 
     ExceptionMessage(String message) {
